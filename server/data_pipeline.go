fe062b4208fceed01447b24146fa21c8a1ab9dd5
Navaz Alani <nalani@uwaterloo.ca>
2020-11-24 03:16:37 -0500

    formatting

--- server/data_pipeline.go
+++ server/data_pipeline.go
@@ -39,18 +39,18 @@ func (d *DataPipeline) AddTransform(transform BufferTransform, pipelineName stri
 }
 
 func (d *DataPipeline) Process(pipelineName string, data []byte) ([]byte, error) {
-  if pipelines, ok := d.pipelines[pipelineName]; !ok {
-    return data, fmt.Errorf("pipeline non-existent")
-  } else {
-    var ctx TransformContext
-    var err error
-    for _, transform := range pipelines {
-      if data, err = transform(ctx, data); err != nil {
-        return data, fmt.Errorf("pipeline error: "+err.Error())
-      } else if ctx.Stat == -1 {
-        return data, fmt.Errorf("pipeline terminated: "+ctx.Msg)
-      }
-    }
-  }
+	if pipelines, ok := d.pipelines[pipelineName]; !ok {
+		return data, fmt.Errorf("pipeline non-existent")
+	} else {
+		var ctx TransformContext
+		var err error
+		for _, transform := range pipelines {
+			if data, err = transform(ctx, data); err != nil {
+				return data, fmt.Errorf("pipeline error: " + err.Error())
+			} else if ctx.Stat == -1 {
+				return data, fmt.Errorf("pipeline terminated: " + ctx.Msg)
+			}
+		}
+	}
 	return data, nil
 }
