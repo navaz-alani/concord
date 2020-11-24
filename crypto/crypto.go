1356efb90e4f925c91f0ffab6ba024effd13ee4d
Navaz Alani <nalani@uwaterloo.ca>
2020-11-24 03:17:46 -0500

    removed unnecessary code

--- crypto/crypto.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/navaz-alani/concord/server"
-
-// Cipher is a ServerExtension which performs encyrption on outgoing packets.
-type Cipher interface {
-	server.ServerExtension
-}
