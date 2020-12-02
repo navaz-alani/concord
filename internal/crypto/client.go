bcc6c407a9400bbf7cdafc668250c44d7ee12029
Navaz Alani <nalani@uwaterloo.ca>
2020-12-02 15:51:18 -0500

    docs

--- internal/crypto/client.go
+++ internal/crypto/client.go
@@ -28,7 +28,7 @@ func (cr *Crypto) IsKeyExchanged(addr string) bool {
 // client will be secure i.e. packets sent between the server and the client
 // will be encrypted with AES, using a shared key generated using ECDH. The
 // `pkt` parameter will be used to compose the key exchange packet with the
-// server.
+// server (ownership of `pkt` is assumed by ConfigureClient).
 func ConfigureClient(client client.Client, svrAddr string, pkt packet.Packet) (*Crypto, error) {
 	// generate private key
 	privKey, err := ecdsa.GenerateKey(Curve, rand.Reader)
@@ -54,8 +54,10 @@ func ConfigureClient(client client.Client, svrAddr string, pkt packet.Packet) (*
 // ConfigureKeyExClientPkt writes the configuration (target, metadata, body,
 // etc) for a client-client key-exchange with the given address to the packet.
 // The packet being written to must be new and after a call to this method, the
-// packet's Write method should not be used.
+// packet's Write method should not be used. Also, this method will clear pw's
+// underlying Packet.
 func (cr *Crypto) ConfigureKeyExClientPkt(addr string, pw packet.Writer) {
+	pw.Clear()
 	pw.Meta().Add(packet.KeyTarget, TargetKeyExchangeClient)
 	pw.Write([]byte(fmt.Sprintf(`{"ip":"%s"}`, addr)))
 	pw.Close()
@@ -64,7 +66,9 @@ func (cr *Crypto) ConfigureKeyExClientPkt(addr string, pw packet.Writer) {
 // ConfigureKeyExServerPkt writes the configuration (target, metadata, body, etc)
 // for a client-server key-exchange. The packet being written to must be new and
 // after a call to this method, the packet's Write method should not be used.
+// Also, this method will clear pw's underlying packet.
 func (cr *Crypto) ConfigureKeyExServerPkt(pw packet.Writer) {
+	pw.Clear()
 	pw.Meta().Add(packet.KeyTarget, TargetKeyExchangeServer)
 	pw.Write(cr.publicKey)
 	pw.Close()
@@ -72,11 +76,11 @@ func (cr *Crypto) ConfigureKeyExServerPkt(pw packet.Writer) {
 
 // EncryptFor encrypts the given data for the given address using the
 // corresponding shared key. If there is no shared key, or there is an
-// encryption error, the error returned will be non-nil an reflect this.
+// encryption error, the error returned will be non-nil and reflect this.
 //
-// It is used in end-to-end encryption to encrypt outgoing payloads, destined to
-// the address. To generate a shared key with the address, a client-client key
-// exchange has to be performed.
+// It is used in end-to-end encryption to encrypt outgoing payloads, being
+// server-relayed to `addr`. To generate a shared key with `addr`, a
+// client-client key exchange has to be performed.
 func (cr *Crypto) EncryptFor(addr string, data []byte) ([]byte, error) {
 	if ks, ok := cr.getKeyStore(addr); !ok {
 		return nil, fmt.Errorf("keys not exchanged")
@@ -87,13 +91,13 @@ func (cr *Crypto) EncryptFor(addr string, data []byte) ([]byte, error) {
 	}
 }
 
-// DecryptFrom decrypts the given data using the shared key with the given
-// address. The returned error is non-nil if there is no shared key between with
-// the address and if there is an error in the decryption process.
+// DecryptFrom decrypts the given data using the shared key with `addr. The
+// returned error is non-nil if there is no shared key between with the address
+// and if there is an error in the decryption process.
 //
-// It is used in end-to-end encryption to decrypt incoming payloads, from the
-// given address. To generate a shared key with the address, a client-client key
-// exchange has to be performed.
+// It is used in end-to-end encryption to decrypt incoming payloads, which have
+// been server-relayed from the given address. To generate a shared key with
+// `addr`, a client-client key exchange has to be performed.
 func (cr *Crypto) DecryptFrom(addr string, data []byte) ([]byte, error) {
 	if ks, ok := cr.getKeyStore(addr); !ok {
 		return nil, fmt.Errorf("keys not exchanged")
@@ -107,7 +111,11 @@ func (cr *Crypto) DecryptFrom(addr string, data []byte) ([]byte, error) {
 // EncryptE2E performs encryption on the packet's data for end-to-end encryption
 // during transit to the packet's destination. The packet is modified so that
 // its Data method returns the encrypted data. Clients should use this to
-// encrypt packets.
+// encrypt their application data. Note that E2E does not imply that the entire
+// packet is encrypted since the packet metadata will still possibly be plain
+// text. To get full-packet encryption, clients should install Crypto on their
+// `client` handle, and perform a key-exchange with they servers they interact
+// with to get full-packet encryption between the client and the servers.
 //
 // Note that before this function can work, it needs a shared key with the
 // destination to which the packet is destined. If a key exchange has been
@@ -160,7 +168,13 @@ func (cr *Crypto) ProcessKeyExResp(addr string, resp packet.Packet) error {
 }
 
 // ServerKEx performs a key exchange between the client and the server at
-// svrAddr.
+// svrAddr. If successful, there will then exist a shared key between `svrAddr`
+// and `client`. A side effect of this is that if the Crypto extension is
+// installed on `client`, then all packets whose Dest method returns `svrAddr`,
+// will have their content encrypted in transit. This is a full-packet
+// encryption (metadata and data), in contrast to end-to-end encryption which is
+// just a data encrytion i.e. the packet's metadata will still possibly be in
+// plain text.
 func (cr *Crypto) ServerKEx(client client.Client, svrAddr string, pkt packet.Packet) error {
 	cr.ConfigureKeyExServerPkt(pkt.Writer())
 	kexResp := make(chan packet.Packet)
@@ -172,7 +186,10 @@ func (cr *Crypto) ServerKEx(client client.Client, svrAddr string, pkt packet.Pac
 }
 
 // ClientKEx performs a key exchange between the client and the client at
-// clientAddr.
+// clientAddr. If successful, there will then exist a shared key between
+// `clientAddr` (remote) and `client` (local). This shared key can be used for
+// end-to-end encryption with `clientAddr` with the {Encrypt,Decrypt}E2E
+// methods.
 func (cr *Crypto) ClientKEx(client client.Client, clientAddr string, pkt packet.Packet) error {
 	cr.ConfigureKeyExClientPkt(clientAddr, pkt.Writer())
 	respChan := make(chan packet.Packet)
